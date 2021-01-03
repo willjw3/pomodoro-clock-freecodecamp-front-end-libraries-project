32fff48aea6ea29a7d785f9dbd60d9aa4dd07c45
willjw3 <will.j.ward3@gmail.com>
2021-01-03 23:53:55 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header"
+import ClockBody from "./components/clockbody"
+import Footer from "./components/footer"
+import "fontsource-orbitron"
+import "fontsource-courgette"
+import "fontsource-vt323"
+import "fontsource-share-tech-mono"
+import "./styles/app.css"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <div className="app-main">
+        <ClockBody />
+      </div>
+      <Footer />
     </div>
   );
 }
